daa77e0cc3dab5fd7cf7817596317cdbbc810193
Sharat M R <cosmosgenius@gmail.com>
2016-12-27 02:03:26 +0530

    Add test

---
+++ test.js
@@ -0,0 +1,19 @@
+const nock = require('nock');
+const request = require('supertest-as-promised');
+const app = require('./lib');
+
+describe('twitter-proxy api',function () {
+    it('api should hit the /test/testme url', async () => {
+        const twitterapi = nock('https://api.twitter.com/test/testme')
+                            .get('/')
+                            .reply(200, {
+                                test: 'success'
+                            });
+
+        const res = await request(app.listen())
+            .get('/test/testme/')
+            .expect(200, {
+                test: 'success'
+            });
+    });
+});
